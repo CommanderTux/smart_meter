399b9d9ab380a714f5ab328573522e7e51f5fdc6
Commander Tux <44753149+CommanderTux@users.noreply.github.com>
2020-12-29 15:42:37 +0100

    Add files via upload

---
+++ Hosts.h
@@ -0,0 +1,8 @@
+#define INFLUXDB_STRATO "xxx.xxx.xxx.xxx"  // Influxdb Strato
+#define INFLUXDB_UBUNTU "xxx.xxx.xxx.xxx"  // Influxdb Ubuntu 
+#define INFLUXDB_USER "********"
+#define INFLUXDB_PASS "********"
+#define INFLUXDB_DATABASE "smart_meter"    // "smart_meter"
+
+#define WIFI_SSID "***********"            // SSID for iot devices
+#define WIFI_PASS "***********"            // Password for iot devices
